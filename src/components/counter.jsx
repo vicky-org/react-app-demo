68d181ed324b3163ce606eeef75c57509d7580d1
Vignesh <vignesh.krishnaraj@entropiktech.com>
2023-09-18 12:49:44 +0530

    added badgeclass

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,17 +4,20 @@ class Counter extends Component {
     state = {
         count: 0,
     };
-    styles = {
-        fontSize: 20,
-        fontWeight: "bold"
-    };
+
     render() { 
         return (
         <div>
-            <span style={{fontSize: 20}} className='badge badge-primary m-2'>{this.formatCount()}</span>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className='btn btn-secondary btn-sm'>Increment</button>
         </div>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
 
     formatCount() {
