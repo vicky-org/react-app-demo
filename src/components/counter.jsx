3cce193483c3fca2a11d5adf6b8e09c460b07712
Vignesh <vignesh.krishnaraj@entropiktech.com>
2023-09-18 21:20:46 +0530

    passing children as props

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -15,8 +15,11 @@ class Counter extends Component {
   };
 
   render() {
+    console.log(this.props);
     return (
       <div>
+        {this.props.children}
+        <h4>{this.props.id}</h4>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           //onClick={this.doHandleIncrement()}
