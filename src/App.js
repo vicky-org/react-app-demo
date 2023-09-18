8b13133221a2f3ee6de3f1b3c35653a59eea5f53
Vignesh <vignesh.krishnaraj@entropiktech.com>
2023-09-18 12:42:16 +0530

    initial commit with styles example

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> love youuu di chellam.
+            Edit <code>src/App.js</code> love youuu di chellam ammu.
           </p>
           <a
             className="App-link"
---
+++ src/components/counter.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        count: 0,
+    };
+    styles = {
+        fontSize: 20,
+        fontWeight: "bold"
+    };
+    render() { 
+        return (
+        <div>
+            <span style={{fontSize: 20}} className='badge badge-primary m-2'>{this.formatCount()}</span>
+            <button className='btn btn-secondary btn-sm'>Increment</button>
+        </div>
+        );
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
+}
+ 
+export default Counter;
