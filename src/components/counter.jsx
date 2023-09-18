36251756f77d9eaee86002bfd44339e4f59afee8
Vignesh <vignesh.krishnaraj@entropiktech.com>
2023-09-18 15:48:14 +0530

    passing event arguments

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,8 +10,13 @@ class Counter extends Component {
   //     this.handleIncrement = this.handleIncrement.bind(this);
   //   }
 
-  handleIncrement = () => {
+  handleIncrement = (product) => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
+  };
+
+  doHandleIncrement = () => {
+    this.handleIncrement({ id: 1 });
   };
 
   render() {
@@ -19,7 +24,8 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          //onClick={this.doHandleIncrement()}
+          onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
