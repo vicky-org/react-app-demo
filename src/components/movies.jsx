24f2e7ed4f2941eb6fac7dbdf66409f0dbd11671
Vignesh <vignesh.krishnaraj@entropiktech.com>
2023-09-18 17:27:14 +0530

    deleting a movie

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -7,24 +7,71 @@ class Movies extends Component {
   };
 
   handleDelete = (movie) => {
-    this.state.movies.pop();
-    this.setState({ movies: this.state.movies });
+    console.log(movie);
+    const movies = this.state.movies.filter((mov) => mov._id !== movie._id);
+    this.setState({ movies });
   };
 
   render() {
     return (
-      <div>
-        <h1>{this.getDisplayMessage()}</h1>
-        <button
-          //onClick={this.doHandleIncrement()}
-          onClick={() => this.handleDelete({ id: 1 })}
-          className="btn btn-secondary btn-sm"
-        >
-          Delete
-        </button>
-      </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.movies.map((movie) => (
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     );
   }
+
+  //   title: "Gone Girl",
+  //   genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+  //   numberInStock: 7,
+  //   dailyRentalRate: 4.5,
+
+  // {this.state.movies.map((movie) => (
+  //     <tr>
+  //       <td>{movie.title}</td>
+  //       <td>{movie.genre}</td>
+  //       <td>{movie.numberInStock}</td>
+  //       <td>{movie.dailyRentalRate}</td>
+  //     </tr>
+  //   ))}
+
+  // table.table>thead>tr>th*44 press tab
+  // tbodyd>tr>td*44
+  //   <div>
+  //         <h1>{this.getDisplayMessage()}</h1>
+  //         <button
+  //           //onClick={this.doHandleIncrement()}
+  //           onClick={() => this.handleDelete({ id: 1 })}
+  //           className="btn btn-secondary btn-sm"
+  //         >
+  //           Delete
+  //         </button>
+  //       </div>
 
   getDisplayMessage() {
     const { movies } = this.state;
