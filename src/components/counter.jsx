ac27f4fdf606afa694b232a600959ab8334f28e4
Vignesh <vignesh.krishnaraj@entropiktech.com>
2023-09-19 13:04:26 +0530

    raising and handling events and updating state in counters

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,31 +2,32 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
   };
 
-  handleIncrement = (product) => {
-    console.log(product);
+  handleIncrement() {
     this.setState({ value: this.state.value + 1 });
-  };
+  }
 
-  doHandleIncrement = () => {
-    this.handleIncrement({ id: 1 });
-  };
+  handleDecrement() {
+    this.setState({ value: this.state.value - 1 });
+  }
 
   render() {
-    console.log(this.props);
     return (
       <div>
-        {this.props.children}
-        <h4>{this.props.id}</h4>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          //onClick={this.doHandleIncrement()}
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.handleIncrement()}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,13 +10,22 @@ class Counters extends Component {
       { id: 4, value: 0 },
     ],
   };
+
+  handleDelete = (counterId) => {
+    console.log("Event handler called", counterId);
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} value={counter.value} id={counter.id}>
-            <h4>Counter #{counter.id}</h4>
-          </Counter>
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter}
+          />
         ))}
       </div>
     );
