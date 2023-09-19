01b97a8b93f16837ed0b10fee59ad85e2ea00b93
Vignesh <vignesh.krishnaraj@entropiktech.com>
2023-09-19 16:51:02 +0530

    added like component

---
+++ src/components/common/like.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+// Input: liked: boolean
+// Output: onClick
+const Like = (props) => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
+  return (
+    <i
+      onClick={props.onClick}
+      style={{ cursor: "pointer" }}
+      className={classes}
+      aria-hidden="true"
+    ></i>
+  );
+};
+
+export default Like;
+
+// class Like extends Component {
+//   render() {
+//     let classes = "fa fa-heart";
+//     if (!this.props.liked) classes += "-o";
+//     return (
+//       <i
+//         onClick={this.props.onClick}
+//         style={{ cursor: "pointer" }}
+//         className={classes}
+//         aria-hidden="true"
+//       ></i>
+//     );
+//   }
+// }
+
+// export default Like;
--- src/services/fakeMovieService.js
+++ src/services/fakeMovieService.js
@@ -8,6 +8,7 @@ const movies = [
     numberInStock: 6,
     dailyRentalRate: 2.5,
     publishDate: "2018-01-03T19:04:28.809Z",
+    liked: true,
   },
   {
     _id: "5b21ca3eeb7f6fbccd471816",
@@ -43,6 +44,7 @@ const movies = [
     genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
     numberInStock: 7,
     dailyRentalRate: 3.5,
+    liked: true,
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181e",
