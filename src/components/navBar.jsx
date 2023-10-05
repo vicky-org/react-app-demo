59c582eda08865e82fc0f50cf15dee092da86298
Vignesh <vignesh.krishnaraj@entropiktech.com>
2023-10-06 00:53:47 +0530

    added navbar and routing logic for vivdly app

---
+++ src/components/navBar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-md navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        Vivdly
+      </Link>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <NavLink className="nav-item nav-link" to="/movies">
+            Movies
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/customers">
+            Customers
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/rentals">
+            Rentals
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -6,9 +6,14 @@ import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.min.css";
 
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
