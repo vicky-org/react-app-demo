c06edbcb26099b8ea6eaa23703a4dc07063d0396
Vignesh <vignesh.krishnaraj@entropiktech.com>
2023-10-06 22:02:57 +0530

    controlled elements for password

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -9,17 +9,18 @@ class LoginForm extends Component {
     e.preventDefault();
 
     //call the server
-    const username = this.username.current.value;
+    const username = this.state.account.username;
     console.log("Form Submitted ", username);
   };
 
-  handleChange = (e) => {
+  handleChange = ({ currentTarget: input }) => {
     const account = { ...this.state.account };
-    account.username = e.currentTarget.value;
+    account[input.name] = input.value;
     this.setState({ account });
   };
 
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -27,8 +28,9 @@ class LoginForm extends Component {
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input
-              value={this.state.account.username}
+              value={account.username}
               onChange={this.handleChange}
+              name="username"
               id="username"
               type="text"
               className="form-control"
@@ -36,7 +38,14 @@ class LoginForm extends Component {
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              name="password"
+              id="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
