71514345ca9202e1fce338d0b72f7c45df094062
Vignesh <vignesh.krishnaraj@entropiktech.com>
2023-10-06 21:01:49 +0530

    added login form

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import "./App.css";
 import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
+            <Route path="/login" component={LoginForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
