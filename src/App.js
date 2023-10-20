5a91a02296525b0067975e433faf21a5c6cd3c02
Vignesh <vignesh.krishnaraj@entropiktech.com>
2023-10-20 11:55:47 +0530

    added register form

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import NavBar from "./components/navBar";
 import "./App.css";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 
 class App extends Component {
   render() {
@@ -18,6 +19,7 @@ class App extends Component {
         <main className="container">
           <Switch>
             <Route path="/login" component={LoginForm} />
+            <Route path="/register" component={RegisterForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
