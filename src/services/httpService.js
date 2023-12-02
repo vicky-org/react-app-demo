7a58be5bb4f4c4154ef46948f8af1318f944897c
Vignesh <vignesh.krishnaraj@entropiktech.com>
2023-12-02 15:51:34 +0530

    fixing bi-directional dependencies

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 import { toast } from "react-toastify";
-import auth from "./authService";
 import logger from "./logService";
 
-axios.defaults.headers.common["x-auth-token"] = auth.getJwt();
+//axios.defaults.headers.common["x-auth-token"] = auth.getJwt();
 
 axios.interceptors.response.use(null, (error) => {
   console.log("Interceptor called da");
@@ -19,9 +18,13 @@ axios.interceptors.response.use(null, (error) => {
   return Promise.reject(error);
 });
 
+function setJwt(jwt) {
+  axios.defaults.headers.common["x-auth-token"] = jwt;
+}
 export default {
   get: axios.get,
   post: axios.post,
   put: axios.put,
   delete: axios.delete,
+  setJwt,
 };
